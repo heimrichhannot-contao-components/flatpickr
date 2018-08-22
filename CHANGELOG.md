1a3672bb70cc405f2d50d22125527940382f7b2c
Thomas Körner <t.koerner@heimrich-hannot.de>
2018-08-22 09:57:35 +0200

    added lib version 4.3.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,17 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
-## [1.1.0] - 2018-08-21
+## [1.2.0] - 2018-08-22
+
+#### Added
+* lib version 4.3.2
+
+## [1.1.0] - 2018-08-22
 
 #### Added
 * lib version 4.2.4
 
-## [1.0.0] - 2018-08-21
+## [1.0.0] - 2018-08-22
 
 #### Added
 * lib version 4.1.4
--- dist/plugins/labelPlugin/plugins/confirmDate/confirmDate.d.ts
+++ dist/plugins/labelPlugin/plugins/confirmDate/confirmDate.d.ts
@@ -1,9 +1,9 @@
-import { Instance } from "../../types/instance";
+import { Plugin } from "../../types/options";
 export interface Config {
     confirmIcon?: string;
     confirmText?: string;
     showAlways?: boolean;
     theme?: string;
 }
-declare function confirmDatePlugin(pluginConfig: Config): (fp: Instance) => {};
+declare function confirmDatePlugin(pluginConfig: Config): Plugin;
 export default confirmDatePlugin;
